cf3095fd1c99f58e67db1bf8737fa0ced4ee71d4
akmindaugas <mindaugas.akelis@gmail.com>
2024-01-27 19:36:13 +0200

    commit JS on main page

---
+++ index.js
@@ -0,0 +1,44 @@
+const logIn = document.getElementById('logInId');
+logInId.addEventListener('click', ()=>{
+    prompt ('login');
+    prompt ('password');
+}) ;
+
+const LtLanguage = document.getElementById('LtContent');
+LtLanguage.addEventListener('click',()=>{
+    console.log ('LT Content');
+const aboutId = document.getElementById('aboutId');
+aboutId.textContent = "Apie mus";
+const productId = document.getElementById('productId');
+productId.textContent = "Produktai";
+const serviceId = document.getElementById('serviceId');
+serviceId.textContent = "Paslaugos";
+const partnersId = document.getElementById('partnersId');
+partnersId.textContent = "Partneriams";
+const logInId = document.getElementById('logInId');
+logInId.textContent = "Prisijungti";
+const contactId = document.getElementById('contactId');
+contactId.textContent = "Susisiekime";
+const h1Id = document.getElementById('h1Id');
+h1Id.textContent = "Programinės įrangos sprendimai verslui";
+
+});
+
+const EnLanguage = document.getElementById('EnContent');
+EnLanguage.addEventListener('click',()=>{
+    console.log ('EN Content');
+const aboutId = document.getElementById('aboutId');
+aboutId.textContent = "About us";
+const productId = document.getElementById('productId');
+productId.textContent = "Products";
+const serviceId = document.getElementById('serviceId');
+serviceId.textContent = "Servise";
+const partnersId = document.getElementById('partnersId');
+partnersId.textContent = "For Partners";
+const logInId = document.getElementById('logInId');
+logInId.textContent = "Login";
+const contactId = document.getElementById('contactId');
+contactId.textContent = "Contact us:";
+const h1Id = document.getElementById('h1Id');
+h1Id.textContent = "Software solutions for business";
+});
