7b73f803cc1a7d6d78c9e90498d7fd12b18d3dcc
akmindaugas <mindaugas.akelis@gmail.com>
2024-01-28 14:01:38 +0200

    commit EN done

--- index.js
+++ index.js
@@ -64,9 +64,25 @@ document.addEventListener('DOMContentLoaded', function () {
         updateElementContent('aboutIdVisionContent', "Musu siekiai: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione excepturi quidem dolorum ducimus nemo eligendi, culpa sapiente blanditiis quasi non, illo commodi? Iure eum, et debitis quasi eveniet temporibus voluptatum dolorum. Perspiciatis, voluptas! Dignissimos facilis perspiciatis iste, illum esse possimus deserunt porro laboriosam omnis quisquam ipsa incidunt error commodi eum ab quas praesentium tempore soluta ");
         updateElementContent('aboutIdFeedback', "Puikus palaikymas ir protinga kaina");
         updateElementContent('aboutIdFeedbackAuthor', "Laimingas užsakovas");
+        ///PRODUCTS MIDDLE SECTION LT-------------------
+        updateElementContent('productsIdh2', "PRODUKTAI, KURIUOS SIŪLOME");
         // updateElementContent('', "");
-        // updateElementContent('', "");
-
+        ///SERVICES MIDDLE SECTION LT -----------
+        updateElementContent('serviceIdh1', "PASLAUGOS, KURIAS MES SIŪLOME");
+        updateElementContent('serviceIdFirst', "FIZINIAMS ASMENIMS");
+        updateElementContent('serviceIdli1', "Siūlome");
+        updateElementContent('serviceIdli2', "Parduodame");
+        updateElementContent('serviceIdli3', "Prižiūrime");
+        updateElementContent('serviceIdli4', "Nuomojame");
+        updateElementContent('serviceIdSecond', "JURIDINIAMS ASMENIMS");
+        updateElementContent('serviceIdli_1', "Siūlome");
+        updateElementContent('serviceIdli_2', "Parduodame");
+        updateElementContent('serviceIdli_3', "Prižiūrime");
+        updateElementContent('serviceIdli_4', "Nuomojame");
+        ///PARTNERS MIDDLE SECTION LT -----------
+        updateElementContent('partnersIdh2', "INFORMACIJA PARTNERIAMS");
+        updateElementContent('partnersIdh3', "Jau greitai");
+     //    updateElementContent('', "");
     });
 
     const EnLanguage = document.getElementById('EnContent');
@@ -89,6 +105,24 @@ document.addEventListener('DOMContentLoaded', function () {
         updateElementContent('aboutIdVisionContent', "Our goals: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione excepturi quidem dolorum ducimus nemo eligendi, culpa sapiente blanditiis quasi non, illo commodi? Iure eum, et debitis quasi eveniet temporibus voluptatum dolorum. Perspiciatis, voluptas! Dignissimos facilis perspiciatis iste, illum esse possimus deserunt porro laboriosam omnis quisquam ipsa incidunt error commodi eum ab quas praesentium tempore soluta ");
         updateElementContent('aboutIdFeedback', "Superior service at reasonable costs");
         updateElementContent('aboutIdFeedbackAuthor', "Happy customer");
+        ///PRODUCTS MIDDLE SECTION EN-------------------
+           updateElementContent('productsIdh2', "PRODCTS WE OFFER");
+        ///SERVICES MIDDLE SECTION EN -----------
+        updateElementContent('serviceIdh1', "SERVICE WE OFFER");
+        updateElementContent('serviceIdFirst', "PRIVATE CUSTOMERS");
+        updateElementContent('serviceIdli1', "Offers");
+        updateElementContent('serviceIdli2', "Sells");
+        updateElementContent('serviceIdli3', "Maintains");
+        updateElementContent('serviceIdli4', "Rents");
+        updateElementContent('serviceIdSecond', "COMPANIES");
+        updateElementContent('serviceIdli_1', "Offers");
+        updateElementContent('serviceIdli_2', "Sells");
+        updateElementContent('serviceIdli_3', "Maintains");
+        updateElementContent('serviceIdli_4', "Rents");
+        ///PARTNERS MIDDLE SECTION EN -----------
+           updateElementContent('partnersIdh2', "INFO FOR PARTNERS");
+           updateElementContent('partnersIdh3', "Comming soon");
+        //    updateElementContent('', "");
     });
 
     function updateElementContent(elementId, newText) {
